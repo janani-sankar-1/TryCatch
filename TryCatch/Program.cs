fcd99d020cec18e11c3a5e88eed2760b115d6d1e
Janani S <Janani S@DESKTOP-13MT9AS>
2024-10-15 19:36:02 +0530

    Add project files.

---
+++ TryCatch/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+namespace TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a, b;
+                Console.Write("enter first number:");
+                string input1 = Console.ReadLine();
+                a = int.Parse(input1);
+                Console.Write("enter second number:");
+                string input2 = Console.ReadLine();
+                b = int.Parse(input2);
+
+                int c = a / b;
+                Console.WriteLine("Result of division" + c);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
